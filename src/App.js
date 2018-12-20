eb5e63e332da204c5d7cc08a88a2f964eb66a534
Janior <janior_orlando@hotmail.com>
2018-12-19 16:04:55 -0800

    Primer reto react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import  { Button } from 'react-bootstrap'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <h1> Hola Mundo </h1>  
+      <div>
+        <h1>Mi primera aplicación</h1>
+        <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+      </div>
     );
   }
 }
---
+++ src/Welcome.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Welcome = () => <h1>Hola mundo desde Component</h1>
+
+export default Welcome;
